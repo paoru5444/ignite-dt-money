23ce6b0c49fa0c6b138dffb92f68b3ffe824a199
Paulo Roberto <betinho5444@gmail.com>
2021-04-17 14:23:47 -0300

    feat: componentizacao and api service modules

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
       --background: #f8f2f5;
       --shape: #ffffff;
       --red: #e52e40;
+      --green: #33cc95;
       --blue: #5429cc;
       --blue-light: #6933ff;
       --text-title: #363f5f;
